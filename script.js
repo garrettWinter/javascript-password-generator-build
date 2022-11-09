b227a12b58ff865568addf6158e3f7fc6b778b4d
Garrett Winter <Garrett_Winter@hotmail.com>
2022-11-09 00:28:02 -0600

    Added the logic to build the string of useable chars for the password.

--- script.js
+++ script.js
@@ -1,77 +1,87 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passwordDetails = writePassword();
+var generateBtn = document.querySelector("#generate"); /*create comments about what this does */
+var passwordDetails = writePassword(); /*create comments about what this does */
 
-/*Create to resolve errors*/
+/* Variables for possible Character sets  */
+var selectedChars;
+
+/*Created to resolve errors.... remove these if possible before submitting*/
 var generatePassword;
 var passwordText;
 var passwordRequirements;
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialChar = "!@#$%^&*()";
+  var selectedChars = '';
   var passwordRequirements = {  
     allowLowerCase: window.confirm("Can lower case letters be used?"),
     allowUpperCase: window.confirm("Can upper case letters be used?"),
     allowNumbers: window.confirm("Can numbers be used?"),
     allowSymbols: window.confirm("Can special characters and symbols be used?"),
-    passwordLength: window.prompt("Please define passord length. (Must be bewtween 8 and 128 characters.)")
+    passwordLength: window.prompt("Please define password length. (Must be between 8 and 128 characters.)")
   }
+
   while (passwordRequirements.passwordLength < 8 ){
-    passwordRequirements.passwordLength = window.prompt("Password is too short, please select a number bewtween 8 and 128 characters.)");
+    passwordRequirements.passwordLength = window.prompt("Password is too short, please select a number between 8 and 128 characters.)");
   }
+
   while (passwordRequirements.passwordLength > 128 ){
-    passwordRequirements.passwordLength = window.prompt("Password is too long, please select a number bewtween 8 and 128 characters.)");
+    passwordRequirements.passwordLength = window.prompt("Password is too long, please select a number between 8 and 128 characters.)");
   }
-   // for (let passwordRequirements.passwordLength < 8;) {
-  //   passwordRequirements.passwordLength = window.prompt("Password is too short, please select a number bewtween 8 and 128 characters.")
-     
-  //   }
 
-  //   for (let index = 0; index < array.length; index++) {
-      
-  //   }
-  
-  
-      /*update this section to be a for loop it is only checking once..... must added in too large after that*/
-      // if (passwordRequirements.passwordLength) {
-      //   passwordRequirements.passwordLength < 8;
-      //     passwordRequirements.passwordLength = window.prompt("Not acceptable length, please select between 8 and 128 characters.)")
-      // }
+  console.log("Written as part of writePassword Logic");
+  console.log(passwordRequirements);
 
+/* If statments to build out the string of useable */
+console.log(passwordRequirements.allowLowerCase);
+  if (passwordRequirements.allowLowerCase == true) {
+    console.log("allowlowercaseworking="+passwordRequirements.allowLowerCase);
+    console.log("Added Chars:"+lowerCase);
+    selectedChars += lowerCase;
+  }
 
+  if (passwordRequirements.allowUpperCase == true) {
+    console.log("allowUpperCase="+passwordRequirements.allowUpperCase);
+    console.log("Added Chars:"+upperCase);
+    selectedChars += upperCase;
+  }
+
+  if (passwordRequirements.allowNumbers == true) {
+    console.log("allowNumbers="+passwordRequirements.allowNumbers);
+    console.log("Added Chars:"+numbers);
+    selectedChars += numbers;
+  }
+
+  if (passwordRequirements.allowSymbols == true) {
+    console.log("allowSymbols="+passwordRequirements.allowSymbols);
+    console.log("Added Chars:"+specialChar);
+    selectedChars += specialChar;
+  }
+
+  console.log("Final Char Set:"+selectedChars)
 
   /*var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;*/
-  console.log("Written as part of writePassword Logic");
-  console.log(passwordRequirements);
   return;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-/* Pseduo coding
-DONE -- Get buttom to on page to work
-DONE -- prompt user for critera and passLength
-Add logic to verify length is 8 to 128 long
-prompt user if should have
-  lower case
-    lowerCase= "abcdefghijklmnopqrstuvwxyz"
-  Uppper case
-    upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  numbers
-    numbers = "0123456789"
-  special charts
-    specialChar= "!@#$%^&*()"
+/* Pseudo coding
+DONE -- Get button to on page to work
+DONE -- prompt user for criteria and passLength
+DONE -- Add logic to verify length is 8 to 128 long
+DONE -- Assemble the char final char set
 Generate password
-Need logic to ensure requriments about are met, if not should reloop
+Need logic to ensure requirements about are met, if not should Reloop
 Display password in an alert
+
+Do we need to have error handling if all chars are selected no?
+
 */
-
-
-
-
-
-
-
